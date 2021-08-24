a51b1812320dfe6be67ee5727246c865452cc0ca
Cleyton Santana <csttn.dev@gmail.com>
2021-08-24 16:57:08 -0300

    init project to java fundamentals

---
+++ src/fundamentos/HelloWorld.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Fala meu amigo, olha tudo que vc passou, e esta de volta. hahahahah");
+
+    }
+}
---
+++ src/fundamentos/Inferencia.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class Inferencia {
+
+    public static void main(String[] args) {
+
+        var n = 4.7;
+//        inferencia de tipo, o java setou o tipo da variavel de acordo com o valor atribuido a mesma.
+        System.out.println(((Object)n).getClass().getSimpleName());
+    }
+}
