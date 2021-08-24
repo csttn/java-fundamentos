9d6a0345edd2ad6e6f50e67fb5ff5518845616fd
Cleyton Santana <csttn.dev@gmail.com>
2021-08-24 17:16:54 -0300

    add tipos primitivos

---
+++ src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+
+
+    byte anosDeEmpresa = 23;
+    short numeroDeVoos = 543;
+    int numero = 586978;
+    long pontosAcumulados = 837483746;
+
+
+//    Por padrão o java adiciona numeros com casas decimais ao tipo double, para especficar que
+//    o valor é do tipo float é necessario colocar o F no final do numero
+    float numeroAleatorio = 3_774.89F;
+
+    double vendasAcumuladas = 3_675_787.78;
+
+    boolean AutoDemissao = false;
+
+//     o tipo char só aceita uma única letra
+    char sexo = 'M';
+
+
+}
